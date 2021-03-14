d4874fb627080d525b8e824c86ebb15ad7854479
ashutr <ashutoshplz99@gmail.com>
2021-03-15 00:26:45 +0530

    implement binary tree insertion

---
+++ CreateBTree.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int item;
+    struct Node* left;
+    struct Node* right;
+};
+typedef struct Node node;
+void insert(node **tree, int val)
+{
+    node *temp = NULL;
+    if(!(*tree))
+    {
+        temp = (node *)malloc(sizeof(node));
+        temp->left = temp->right = NULL;
+        temp->item = val;
+        *tree = temp;
+        return;
+    }
+    if(val < (*tree)->item)
+    {
+        insert(&(*tree)->left,val);
+    }
+    else if(val > (*tree)->item)
+    {
+        insert(&(*tree)->right,val);
+    }
+}
+void DisplayInorder(node *tree)
+{
+    if(tree)
+    {
+        DisplayInorder(tree->left);
+        printf("%d\n",tree->item);
+        DisplayInorder(tree->right);
+    }
+}
+int main()
+{
+    node *root;
+    node *tmp;
+    root  = NULL;
+    insert(&root,9);
+    insert(&root,4);
+    insert(&root,15);
+    insert(&root,6);
+    insert(&root,12);
+    insert(&root,17);
+    insert(&root,2);
+    printf("Display\n");
+    DisplayInorder(root);
+    return 0;
+
+}
